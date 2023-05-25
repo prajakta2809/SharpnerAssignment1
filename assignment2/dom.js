bd8ab37bc9e0bbf74231bfc97fa08cf9f5b38e12
prajakta2809 <prajaktadigraje13@gmail.com>
2023-05-25 20:56:10 +0530

    assignment-3 getElementBuTagName

--- assignment2/dom.js
+++ assignment2/dom.js
@@ -16,8 +16,8 @@
 
 //console.log(document.getElementById('header-title'));
 
-const headerTitle=document.getElementById('header-title');
-const header=document.getElementById('main-header');
+// const headerTitle=document.getElementById('header-title');
+// const header=document.getElementById('main-header');
 
 /*
 console.log(headerTitle);
@@ -25,18 +25,17 @@ headerTitle.textContent='Hello';
 headerTitle.innerText="Goodbye"; //deals with style
 headerTitle.innerHTML='<h3>hye!<h3/>'; //this will go inside h1;this will not change h1 to h3
 */
-header.style.borderBottom='solid 3px black';
 
 //byClassName
 
-var items=document.getElementsByClassName('list-group-item');
+//var items=document.getElementsByClassName('list-group-item');
 /*
 var addItems=document.getElementsByTagName('h2')[0];
 console.log(addItems);
 addItems.style.fontWeight='bold';
 addItems.style.color='green';
 
-*/
+
 
 console.log(items);
 console.log(items[1]);
@@ -45,7 +44,37 @@ for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
 
+//getElement by tag
+var li=document.getElementsByTagName('li');
+
+console.log(li);
+console.log(li[1]);
+li[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    li[i].style.fontWeight='bold';
+}
+*/
+
+//query selector
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px black';
+
+// var input = document.querySelector('input');
+// input.value='hello'; //grabs the first one
+
+// var submit =document.querySelector('input[type="submit"]');
+// submit.value="send";
+
+//--Assignment given
+
+// var li=document.getElementsByTagName('li');
 
 
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
 
- 
+var li=document.getElementsByClassName('list-group-item');
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+}
