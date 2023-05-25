3d752b691263f101c25f500d3e7386a5568d35df
prajakta2809 <prajaktadigraje13@gmail.com>
2023-05-25 13:30:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const ul = document.querySelector('.items');
+
+const btn= document.querySelector('.btn');
+
+// click
+
+// btn.addEventListener('click',(e)=>{
+//   e.preventDefault();
+//   document.querySelector('#my-form').style.background ='#ccc';
+// });
+
+// // mousehover
+
+// btn.addEventListener('mouseover',(e)=>{
+
+//     document.querySelector('body').classList.add('bg-dark');
+// });
+
+// //mouseout
+// btn.addEventListener('mouseout',(e)=>{
+//   document.querySelector('.items').lastElementChild.innerHTML="<h1>Hello<h1/>";
+// });
+
+//2nd task:
+const myForm =document.querySelector('#my-form');
+const nameInput =document.querySelector('#name');
+const email =document.querySelector('#email');
+const user =document.querySelector('#users');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value==='' || email.value===''){
+        console.error("complete the form");
+    }else{
+        console.log(nameInput.value);
+        console.log(email.value);
+    }
+    
+}
