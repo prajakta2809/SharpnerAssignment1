5a3f88571e5403b57063f7f540de51f7c609ca7f
prajakta2809 <prajaktadigraje13@gmail.com>
2023-09-04 20:10:56 +0530

    intro to express

---
+++ Nodejs/expressProject/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express =require('express');
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+}); //for every incoming req
+
+app.use((req,res,next)=>{
+    console.log('In the another middleware');
+    res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(4000);
