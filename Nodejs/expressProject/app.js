c2ce715cb4919eba5a2f01dcc3c051f3863067d9
prajakta2809 <prajaktadigraje13@gmail.com>
2023-09-10 16:31:59 +0530

    intro to middleware assignment

--- Nodejs/expressProject/app.js
+++ Nodejs/expressProject/app.js
@@ -1,14 +1,30 @@
 const http = require('http');
 const express =require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
-}); //for every incoming req
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
+   // console.log('This always runs!');
+   // res.send('<h1>Hello from express</h1>');
+   next();
+});
+
+app.use('/add-product',(req,res,next)=>{
+   // console.log('In the another middleware');
+    res.send('<form action="/product" method="POST"><input   type="text" name="title"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+
+app.post('/product',(req,res,next)=>{
+  
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     console.log('In the another middleware');
     res.send('<h1>Hello from express</h1>');
 });
