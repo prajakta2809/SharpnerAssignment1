e2ea1bd73f6ee20d472033ce29c4ec92dc43b61d
prajakta2809 <prajaktadigraje13@gmail.com>
2024-02-12 15:41:07 +0530

    Login frontend and backend added with status code

--- ExpenseTracker/routes/login.js
+++ ExpenseTracker/routes/login.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const Expense = require('../model/signupModel');
-const loginRoute=require('../routes/login');
+const loginRoute = require('../routes/login');
 
-router.post('/user',(req,res)=>{
-    const {email,password}=req.body;
-    Expense.findOne({where:{email}})
-    .then(existingEmail=>{
-        if(!existingEmail){
-            //console.error(err);
-            res.status(400).send('Email does not exists');
-           // res.render('login',{message:'Email does not exists',flag:true})
-        }
-    })
-    Expense.findOne({where:{email,password}})
-    .then(existingUser=>{
-        if(!existingUser){
-           // console.error(err);
-            res.status(401).send('Invalid password');
-           // return res.render('login',{message:'Invalid password',flag:false});
-        }
-        else{
-            return res.render('login',{message:'Login success',flag:false});
-        }
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-      });
-    
+router.post('/user', (req, res) => {
+    const { email, password } = req.body;
+
+    // Find user by email
+    Expense.findOne({ where: { email } })
+        .then(existingUser => {
+            if (!existingUser) {
+                return res.status(404).send('Email does not exist');
+            }
+
+            // Compare passwords
+            bcrypt.compare(password, existingUser.password, (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Internal Server Error');
+                }
+                if (!result) {
+                    return res.status(401).send('Invalid password');
+                } else {
+                    return res.render('login', { message: 'Login success', flag: false });
+                }
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
-router.get('/',(req,res)=>{
-    res.render('login',{message:null,flag:false});
+router.get('/', (req, res) => {
+    res.render('login', { message: null, flag: false });
 });
 
 module.exports = router;
--- ExpenseTracker/routes/signup.js
+++ ExpenseTracker/routes/signup.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const Expense = require('../model/signupModel');
-//const loginRoute=require('../routes/login');
-// router.post('/user/signup', (req, res) => {
-//     const { name, email, password } = req.body;
-//     Expense.create({ name, email, password })
-//       .then(() => {
-//         res.redirect('/');
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).send('Internal Server Error');
-//       });
-//   });
+
 
 router.post('/user/signup', (req, res) => {
   const { name, email, password } = req.body;
@@ -21,14 +11,19 @@ router.post('/user/signup', (req, res) => {
   Expense.findOne({ where: { email } })
     .then(existingUser => {
       if (existingUser) {
-        // If email already exists, send a message to the frontend
        
         //return res.status(400).send('User already exists');
        
         res.render('signup',{message:"User already exists"});
       } else {
+        bcrypt.hash(password,10,(err,hashedPassword)=>{
+          if(err){
+            console.error(err);
+            return res.status(500).send('Internal Server Error');
+          }
+      
         // If email does not exist, create the user
-        Expense.create({ name, email, password })
+        Expense.create({ name, email, password: hashedPassword })
           .then(() => {
             res.redirect('/');
           })
@@ -36,8 +31,10 @@ router.post('/user/signup', (req, res) => {
             console.error(err);
             res.status(500).send('Internal Server Error');
           });
+        });
       }
-    })
+    
+  })
     .catch((err) => {
       console.error(err);
       res.status(500).send('Internal Server Error');
